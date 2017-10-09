ff2b49c411657622e2a5bb1aa770d3692eb6f801
thebrianemory <the@brianemory.com>
2017-10-09 15:47:15 -0700

    Finish Stateful Server Processes

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,12 +1,20 @@
 defmodule Servy.PledgeServer do
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_servie(name, amount)
 
-    [ {"larry", 10} ]
-  end
+  def listen_loop(state) do
+    IO.puts "\nWaiting for a message..."
 
-  def recent_pledges do
-    [ {"larry", 10} ]
+    receive do
+      {:create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [ {name, amount} | state ]
+        IO.puts "#{name} pledged #{amount}"
+        IO.puts "New state is #{inspect new_state}"
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        IO.puts "Sent pledges to #{inspect sender}"
+        listen_loop(state)
+    end
   end
 
   defp send_pledge_to_service(_name, _amount) do
