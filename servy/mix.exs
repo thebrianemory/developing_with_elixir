06410a400d7849566bdeb7edf966dd2a37bf6ff7
thebrianemory <the@brianemory.com>
2017-10-10 18:22:18 -0700

    Finish Final OTP Application (end of the course)

--- servy/mix.exs
+++ servy/mix.exs
@@ -4,6 +4,7 @@ defmodule Servy.Mixfile do
   def project do
     [
       app: :servy,
+      description: "A humble HTTP server",
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
@@ -14,7 +15,9 @@ defmodule Servy.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Servy, []},
+      env: [port: 3000]
     ]
   end
 
